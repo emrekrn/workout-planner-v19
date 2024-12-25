7590cb2c71d8ba9a9434eb51c39663b25ed9ab78
emrekrn <136122266+emrekrn@users.noreply.github.com>
2024-12-25 15:42:22 +0100

    PrimeNg, TailwindCss and prettier has been installed. Poc can be found in app.component.html.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Button } from 'primeng/button';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	imports: [RouterOutlet, Button],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'workout-planner-v19';
+	title = 'workout-planner-v19';
 }
