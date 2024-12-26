e46b113e07a6b4b6bf693158d5a4c8f46140857d
emrekrn <136122266+emrekrn@users.noreply.github.com>
2024-12-26 12:43:52 +0100

    Dashboard page has been added to routes and protected by authGuard.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AuthGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
-	{ path: '', component: LoginPageComponent, pathMatch: 'full' },
+	{
+		path: 'dashboard',
+		component: DashboardComponent,
+		canActivate: [AuthGuard],
+	},
+	{ path: 'login', component: LoginPageComponent, pathMatch: 'full' },
+	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
